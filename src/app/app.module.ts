f7c807694fd09632d4c231f1a0771030d803709d
parth0245 <parth5502@gmail.com>
2023-01-17 19:55:30 +0530

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AirQualityComponent } from './components/air-quality/air-quality.component';
+import { TemperatureComponent } from './components/temperature/temperature.component';
+import { AudioComponent } from './components/audio/audio.component';
+import { DisplaysComponent } from './components/displays/displays.component';
+import { LightingComponent } from './components/lighting/lighting.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AirQualityComponent,
+    TemperatureComponent,
+    AudioComponent,
+    DisplaysComponent,
+    LightingComponent
   ],
   imports: [
     BrowserModule,
